e4f2e0a8904863d6dc8401783d32662f44185c77
Monil Raycha <monilraycha53@gmail.com>
2025-01-23 11:57:26 +0530

    first commit

---
+++ android/app/src/main/java/com/firebaseapp/ApplicationClass.java
@@ -0,0 +1,6 @@
+package com.firebaseapp;
+
+import android.app.Application;
+
+public class ApplicationClass extends Application {
+}
---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by MOnil on 16/01/25.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
